0fac0bf144897e700c6470091cfdbc72e7e37991
Fiorella Larriviere <fcastillolarriviere@gmail.com>
2022-11-18 22:49:09 -0300

    copia todo - reemplazo old_price

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -3,7 +3,13 @@ import { Application } from "@hotwired/stimulus"
 const application = Application.start()
 
 // Configure Stimulus development experience
+
+
+
 application.debug = false
 window.Stimulus   = application
 
+import { Slideover } from "tailwindcss-stimulus-components"
+application.register('slideover', Slideover)
+export { Slideover}
 export { application }
