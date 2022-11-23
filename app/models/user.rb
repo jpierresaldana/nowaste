d9bd62feb196db8762972b571dff801955248498
Jean Pierre <jp.saldana@outlook.es>
2022-11-22 21:09:28 -0500

    algunos cambios

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :omniauthable, :omniauth_providers => [:google_oauth2]
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :omniauthable,
+  :omniauth_providers => [:google_oauth2]
   has_many :products
   has_one_attached :photo
 
